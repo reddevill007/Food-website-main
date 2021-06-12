dc22965232c40f71418beb705cdd177dc3e3ff00
Saurabh Pandey <74395114+reddevill007@users.noreply.github.com>
2021-06-12 23:16:34 +0530

    Add files via upload
    I commit to this repository

---
+++ aap.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $('.food-slider').slick({
+        autoplay:true,
+        slidesToShow:3,
+        slidesToScroll:1,
+        prevArrow:".prev-btn",
+        nextArrow:".next-btn",
+    });
+});
